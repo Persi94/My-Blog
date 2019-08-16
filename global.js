4bae79311605380fc62fe7591a15bd1b5846db7b
Nick <persi94gn@gmail.com>
2019-08-16 17:07:10 +0500

    problem with z-index

---
+++ global.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+    var yellowBall = document.getElementById('yellow-ball');
+    yellowBall.setAttribute('class','grow-small-yellow-and-green-balls');
+
+    var redBall = document.getElementById('red-ball');
+    redBall.setAttribute('class','grow-medium-red-and-blue-balls');
+
+    var userBall = document.getElementById('user-ball');
+    userBall.setAttribute('class','grow-large-purple-ball');
+
+    var blueBall = document.getElementById('blue-ball');
+    blueBall.setAttribute('class','grow-medium-red-and-blue-balls');
+
+    var greenBall = document.getElementById('green-ball');
+    greenBall.setAttribute('class','grow-small-yellow-and-green-balls');
+}
