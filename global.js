7c786b16a0e0ca157c8f15193d29d77c7f512979
Nick <persi94gn@gmail.com>
2019-08-17 16:43:38 +0500

    start doing navbar

--- global.js
+++ global.js
@@ -1,16 +1,33 @@
-window.onload = function(){
+document.addEventListener('DOMContentLoaded', function(){
     var yellowBall = document.getElementById('yellow-ball');
-    yellowBall.setAttribute('class','grow-small-yellow-and-green-balls');
+    yellowBall.setAttribute('class','grow-small-x70');
 
     var redBall = document.getElementById('red-ball');
-    redBall.setAttribute('class','grow-medium-red-and-blue-balls');
+    redBall.setAttribute('class','grow-medium-x120');
 
     var userBall = document.getElementById('user-ball');
-    userBall.setAttribute('class','grow-large-purple-ball');
+    userBall.setAttribute('class','grow-large-x200');
 
     var blueBall = document.getElementById('blue-ball');
-    blueBall.setAttribute('class','grow-medium-red-and-blue-balls');
+    blueBall.setAttribute('class','grow-medium-x120');
 
     var greenBall = document.getElementById('green-ball');
-    greenBall.setAttribute('class','grow-small-yellow-and-green-balls');
-}
+    greenBall.setAttribute('class','grow-small-x70');
+});
+
+// window.onload = function(){
+//     var yellowBall = document.getElementById('yellow-ball');
+//     yellowBall.setAttribute('class','grow-small-x70');
+
+//     var redBall = document.getElementById('red-ball');
+//     redBall.setAttribute('class','grow-medium-x120');
+
+//     var userBall = document.getElementById('user-ball');
+//     userBall.setAttribute('class','grow-large-x200');
+
+//     var blueBall = document.getElementById('blue-ball');
+//     blueBall.setAttribute('class','grow-medium-x120');
+
+//     var greenBall = document.getElementById('green-ball');
+//     greenBall.setAttribute('class','grow-small-x70');
+// }
