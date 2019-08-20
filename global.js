93864cb9f29b3e5cba222d1f733c3fbe9e567958
Nick <persi94gn@gmail.com>
2019-08-20 12:08:18 +0500

    scroll function

--- global.js
+++ global.js
@@ -15,6 +15,19 @@ document.addEventListener('DOMContentLoaded', function(){
     greenBall.setAttribute('class','grow-small-x70');
 });
 
+document.addEventListener('scroll',function(){
+    if(document.body.scrollTop > 350 || document.documentElement.scrollTop > 350){
+        document.getElementById('navbar-menu').removeAttribute('class','navbar-no-changed');
+        document.getElementById('navbar-menu').setAttribute('class','navbar-change-on-scroll');
+        document.getElementById('home-button').setAttribute('class','navbar-home-button-change-on-scroll');
+    }
+    else{
+        document.getElementById('navbar-menu').removeAttribute('class','navbar-change-on-scroll');
+        document.getElementById('home-button').removeAttribute('class','navbar-home-button-change-on-scroll');
+        document.getElementById('navbar-menu').setAttribute('class','navbar-no-changed');
+    }
+});
+
 // window.onload = function(){
 //     var yellowBall = document.getElementById('yellow-ball');
 //     yellowBall.setAttribute('class','grow-small-x70');
