c681adc2b93c0d08a6485e109258cc9229eb0dcc
Nick <persi94gn@gmail.com>
2019-09-14 16:35:19 +0500

    slide show

--- global.js
+++ global.js
@@ -1,9 +1,9 @@
 var slideChilds = document.getElementById('slide-show').children;
+var dotsChilds = document.getElementById('slide-dots').children;
+
 var i = 0;
 setInterval(function(){
-    if(i < slideChilds.length){
-        slideChilds[i].classList.add('hide-slide');
-        slideChilds[i+1].classList.remove('hide-slide');
-    }
-    i++;
-},2000)
+    slideChilds[i].classList.add('hide-slide');
+    i = (i+1)%slideChilds.length;
+    slideChilds[i].classList.remove('hide-slide');
+},3000)
