1820fdebc750b4ad840a9b80403e4750eea07353
Nick <persi94gn@gmail.com>
2019-09-27 18:43:11 +0500

    calc2

--- js/calc.js
+++ js/calc.js
@@ -1,43 +1,39 @@
-// var display = document.getElementById('num-display');
-// var numArray = [];
+var display = document.getElementById('num-display');
+var buttons = document.getElementsByTagName('td');
+var signs = ['+','-','*','/','C','='];
 
-// var add = function addition(){
-//     var n = parseFloat(a) + parseFloat(b);
-//     return n;
-// }
-// var sub = function substraction(){
-//     var n = parseFloat(a) - parseFloat(b);
-//     return n;
-// }
-// var mult = function multiplication(){
-//     var n = parseFloat(a) * parseFloat(b);
-//     return n;
-// }
-// var div = function division(){
-//     var n = parseFloat(a) / parseFloat(b);
-//     return n;
-// }
-
-// function result(){
-//     return
-// }
-
-// function clear(){
-
-// }
-var numArea = document.getElementById('num-display');
-var numBtn = document.getElementsByTagName('td');
-var btnClick = this.innerHTML;
-
-for(var i=0; i<numBtn.length; i++){
-    numBtn[i].onclick = function(e){
-        if(btnClick === 'C'){
-            numArea.innerHTML = '0';
-        }else if(btnClick === '='){
-            numArea.innerHTML = eval(numArea);
-        }else{
-            numArea.innerHTML += btnClick; 
+for(var i=0; i<buttons.length; i++){
+    if(buttons[i].innerHTML === signs[4]){
+        buttons[i].onclick = function(){
+            display.innerHTML = '0';
         }
-        e.preventDefault();
+    }else if(buttons[i].innerHTML === signs[5]){
+        buttons[i].onclick = function(){
+            if(display.innerHTML.includes(signs[0])){
+                var numString = display.innerHTML.split(signs[0]);
+                var result = parseFloat(numString[0]) + parseFloat(numString[1]);
+            }
+            if(display.innerHTML.includes(signs[1])){
+                var numString = display.innerHTML.split(signs[1]);
+                var result = parseFloat(numString[0]) - parseFloat(numString[1]);
+            }
+            if(display.innerHTML.includes(signs[2])){
+                var numString = display.innerHTML.split(signs[2]);
+                var result = parseFloat(numString[0]) * parseFloat(numString[1]);
+            }
+            if(display.innerHTML.includes(signs[3])){
+                var numString = display.innerHTML.split(signs[3]);
+                var result = parseFloat(numString[0]) / parseFloat(numString[1]);
+            }
+            display.innerHTML = result;
+        }
+    }else{
+        buttons[i].onclick = function(){
+            if(display.innerHTML === '0'){
+                display.innerHTML = this.innerHTML;
+            }else{
+                display.innerHTML += this.innerHTML;
+            }
+        }
     }
 }
